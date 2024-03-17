df7ef3ea2afcb843f1ce37e21cb0dde05c2d94bf
tgrangeo <thomas.grangeon9@gmail.com>
2024-03-17 22:02:25 +0100

    init

---
+++ Resistor_color/main.ts
@@ -0,0 +1,15 @@
+export const colorCode = (str: string) => {
+  return COLORS.indexOf(str);
+};
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
---
+++ Two_Fer/main.ts
@@ -0,0 +1,4 @@
+export function twoFer(name: string | undefined): string {
+  if (name) return `One for ${name}, one for me.`;
+  else return `One for you, one for me.`;
+}
