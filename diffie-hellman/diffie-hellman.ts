f16229e1243cdf75ebad2a1bc91b5c97e0e20736
tgrangeo <thomas.grangeon9@gmail.com>
2024-04-24 22:45:29 +0200

    hi

--- diffie-hellman/diffie-hellman.ts
+++ diffie-hellman/diffie-hellman.ts
@@ -2,7 +2,7 @@ export class DiffieHellman {
   p!: number;
   g!: number;
   n!: number;
-  totient!:number;
+  totient!: number;
 
   isPrime = (num: number) => {
     for (let i = 2, s = Math.sqrt(num); i <= s; i++) {
@@ -11,23 +11,38 @@ export class DiffieHellman {
     return num > 1;
   };
 
+  isPrimitiveRoot = (g: number, p: number) => {
+    const requiredSet = new Set<number>();
+    for (let x = 1; x < p; x++) {
+      requiredSet.add(Math.pow(g, x) % p);
+    }
+    return requiredSet.size === p - 1;
+  };
+
   constructor(p: number, g: number) {
-    if (p < 1 || p > 9999 || g < 1 || g > 9999) throw "";
-    if (!this.isPrime(p) || !this.isPrime(g)) throw "";
+    if (p < 1 || p > 9999 || g < 1 || g > 9999) throw "Invalid parameters";
+    if (!this.isPrime(p) || !this.isPrimitiveRoot(g, p))
+      throw "g must be a primitive root modulo p and p must be prime";
     this.p = p;
     this.g = g;
     this.n = p * g;
-    this.totient = (p - 1) * ( g - 1)
+    this.totient = (p - 1) * (g - 1);
   }
 
   public getPublicKey(privateKey: number): number {
     if (privateKey < 2 || privateKey >= this.p) {
-      throw "";
+      throw "Invalid private key";
     }
-    return 0;
+    return Math.pow(this.g, privateKey) % this.p;
   }
 
-  public getSecret(theirPublicKey: unknown, myPrivateKey: unknown): unknown {
-    throw new Error("Remove this statement and implement this function");
+  public getSecret(theirPublicKey: number, myPrivateKey: number): number {
+    if (
+      typeof theirPublicKey !== "number" ||
+      typeof myPrivateKey !== "number"
+    ) {
+      throw new Error("Public and private keys must be numbers");
+    }
+    return Math.pow(theirPublicKey, myPrivateKey) % this.p;
   }
 }
